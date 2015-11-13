f05a8742a08253e0968beaedbc5cc8d24e425fd2
DejayJD <jdfrancis@smu.edu>
2015-11-12 18:53:09 -0600

    Updated form and portfolios

--- js/js.js
+++ js/js.js
@@ -10,6 +10,7 @@ $(function() {
 });
 
 $(function(){
+    $(".modal").css({"padding-right":"0"})
     $(window).scroll(function() {
         if ($(window).scrollTop() > 0) {
         $(".navbar").css({
