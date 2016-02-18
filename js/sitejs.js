18aab7bc946d88c59623fdc1c266ab89dd873fc3
DejayJD <jdfrancis@smu.edu>
2016-02-18 11:36:26 -0600

    Updated bio, added headshot picture

--- js/sitejs.js
+++ js/sitejs.js
@@ -9,69 +9,70 @@ $(function() {
     });
 });
 
-$(function(){
-    $(".modal").css({"padding-right":"0"})
+$(function() {
+    $(".modal").css({
+        "padding-right": "0"
+    })
     $(window).scroll(function() {
         if ($(window).scrollTop() > 0) {
             $(".navbar").css({
-                "background-color":"#3B3F48", 
-                "border-bottom": "1px solid #313131", 
+                "background-color": "#3B3F48",
+                "border-bottom": "1px solid #313131",
                 "box-shadow": "0px 1px 2px #242424",
                 "padding-top": "15px",
                 "height": "75px"
-                });
+            });
             $(".lname").css({
-                "margin-left" : "-100px",
-                "visibility" : "hidden",
-                "pointer-events" : "none",
-                "opacity" : "0",
-                "z-index" : "-100",
-                "transition" : "all 0.4s, font-size 10s",
-                "width" : "0px"
+                "margin-left": "-100px",
+                "visibility": "hidden",
+                "pointer-events": "none",
+                "opacity": "0",
+                "z-index": "-100",
+                "transition": "all 0.4s, font-size 10s",
+                "width": "0px"
             });
             $(".fname").css({
-                "background-color" : "#E46849",
-                "z-index" : "100"
+                "background-color": "#E46849",
+                "z-index": "100"
             });
-            myOffset = 75;
-            $(".animate1").each(function(){
-                if ($(window).scrollTop() > $(this).offset().top - $(window).height() - myOffset && $(window).scrollTop() < $(this).offset().top ) {
+            myOffset = 100;
+            $(".animate1").each(function() {
+                if ($(window).scrollTop() > $(this).offset().top - $(window).height() - myOffset && $(window).scrollTop() < $(this).offset().top) {
                     $(this).css({
-                        "opacity" : "1",
-                        "transform" : "translateY(0%)"
+                        "opacity": "1",
+                        "transform": "translateY(0%)"
 
-                    });   
+                    });
                 }
             });
-        }
-        else {
+        } else {
             $(".navbar").css({
                 "background": "none",
                 "border": "none",
-                "box-shadow":"none",
-                "padding-top":"25px",
-                "height":"100px"
+                "box-shadow": "none",
+                "padding-top": "25px",
+                "height": "100px"
             });
             $(".lname").css({
-                "visibility" : "visible",
-                "margin-left" : "-15px",
-                "opacity" : "1",
-                "z-index" : "-100",
-                "width" : "auto",
-                "transition" : "all 0.4s, font-size 0s",
-                "font-size" : "34pt"
+                "visibility": "visible",
+                "margin-left": "-15px",
+                "opacity": "1",
+                "z-index": "-100",
+                "width": "auto",
+                "transition": "all 0.4s, font-size 0s",
+                "font-size": "34pt"
             });
             $(".fname").css({
-                "background" : "none",
-                "z-index" : "100"
-            }); 
+                "background": "none",
+                "z-index": "100"
+            });
         }
-    }); 
+    });
 });
 
 
 //auto expand textarea
 function adjust_textarea(h) {
     h.style.height = "90px";
-    h.style.height = (h.scrollHeight)+10+"px";
+    h.style.height = (h.scrollHeight) + 10 + "px";
 }
--- templates/portfolio-data.js
+++ templates/portfolio-data.js
@@ -1,7 +1,7 @@
 {
     "blocks": [
         {
-            "id": "1",
+            "id": "4",
             "img": "roguelike",
             "imgnum": "3",
             "type": "Game",
@@ -16,7 +16,7 @@
             "description": "This game is a tile-based roguelike made in C++ in Visual Studios 2013 using a simple tile rendering engine and creating everything else from scratch. I learned a lot about how to build a game from the ground up without using engines or large code bases. I also learned a lot about project workflow, about working on code and managing it for months. I learned a lot about code restructuring and refactoring as well as how to write more efficient code. I also used a sloppy code checker that checks for repetitive and inefficient code. This game was a part of a semester long project in Game Development class as a part of the Guildhall at SMU. The class was taught by a Guildhall teacher who is also a full time industry professional."
         },
         {
-            "id": "2",
+            "id": "5",
             "img": "2048game",
             "type": "Game",
             "title": "2048 Minimax AI",
@@ -30,7 +30,7 @@
             "description": "For this project I created a 2048 game clone in Python, and then made an AI to play it. To make the AI I used a Min-Max tree with Alpha-Beta Tree Pruning to calculate moves using a heuristic function that I made. For heuristics I assigned points for the highest number tile and the number of empty spaces. This project is still in development and I am continuing to work on it to make it run more efficiently."
         },
         {
-            "id": "3",
+            "id": "6",
             "img": "garouting",
             "type": "Paper/Software",
             "title": "Partitioning Networks using Genetic Algorithms",
@@ -45,7 +45,7 @@
             "description": "Paper Abstract: In this paper we divised partitions of a network using a genetic algorithm (GA) in order to minimize the mean global slowdown of packet flows throughout the network. Network performance has increasingly become a very integral portion of the quality of services provided over the Internet. Our proposed solution to achieving application-specific performance constraints is to partition networks according to the lengths of packet flows serviced at each routing device. We achieved these network partitions using a genetic algorithm implemented in Python. We compared the global mean slowdown of network performance for both unpartitioned networks alongside partitioned networks that were partitioned using our genetic algorithm and we found that networks partitioned with our genetic algorithm reduce the global mean slowdown of network performance with a variety of different packet distributions. We created a simulation in Python and the results show that partitioned networks have the potential to reduce the slowdown of network performance for well-defined packet distributions."
         },
         {
-            "id": "4",
+            "id": "2",
             "img": "appsassins",
             "imgnum": "1",
             "type": "Web",
@@ -61,7 +61,7 @@
             "description": "Appsassins is a more seamless and streamlined way to play the classic game Assassins. The app makes it easier than ever to start and play a game of Assassins with your friends. The app manages assigning targets, confirming targets and more. Rather than having to constantly use other social media intended for other purposes to manage Assassins, we combined it all into one app. You can also view the progress of your games at any time on a visual map overlay. This project was definitely one of my most fun websites to make. Not only was it focused on a game, but I was charged with creating the backend, rather than the front end that I usually make."
         },
         {
-            "id": "5",
+            "id": "1",
             "img": "spots",
             "type": "Web",
             "title": "Spots: the online parking app",
@@ -76,7 +76,7 @@
             "description": "Have you ever attended an event (football game, concert, festival) and tried to avoid high parking prices by parking in residential or commercial lots for sale near the event location? If this is you, then Spots is the app you need to pursue. Spots is a web and mobile app that will help you to find a parking spot at any event in your area. Spots does this through the help of you, the user. As the user of this app, you can register as both a homeowner and a driver. As a homeowner, you indicate how many spots you have available in your lot. This information is readily available to users if they search for an event in your area. As a driver, you register your car and you can reserves spots at a lot before the game as well as pull up the app on game day to find which homes have parking spaces available."
         },
         {
-            "id": "6",
+            "id": "3",
             "img": "portfolio",
             "type": "Web",
             "title": "My Portfolio Website",
